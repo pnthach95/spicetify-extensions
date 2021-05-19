d19cba2a5dc2ac9b7e9d90b7126a0ef0ff0cacbb
Pham Ngoc Thach <thachxyz123@gmail.com>
2021-05-19 09:19:18 +0700

    Update new version

--- romaji.mjs
+++ romaji.mjs
@@ -1,6 +1,9 @@
+/// <reference path="global.d.ts" />
+
 import "./node_modules/kuroshiro/dist/kuroshiro.min.js";
 import "./node_modules/kuroshiro-analyzer-kuromoji/dist/kuroshiro-analyzer-kuromoji.min.js";
 
+const { Type } = Spicetify.URI;
 const kuroshiro = new Kuroshiro();
 kuroshiro.init(new KuromojiAnalyzer());
 
@@ -13,7 +16,31 @@ function converter(input) {
 }
 
 async function showRomaji(uris) {
-  let name = Spicetify.LiveAPI(uris[0])._data.name;
+  const uri = Spicetify.URI.fromString(uris[0]);
+  let data = null;
+  try {
+    switch (uri.type) {
+      case Type.ALBUM:
+        data = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/albums/${uri.getBase62Id()}`);
+        break;
+      case Type.TRACK:
+        data = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/tracks/${uri.getBase62Id()}`);
+        break;
+      case Type.ARTIST:
+        data = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/artists/${uri.getBase62Id()}`);
+        break;
+      case Type.PLAYLIST:
+      case Type.PLAYLIST_V2:
+        data = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/playlists/${uri.getBase62Id()}`);
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log(uri, error);
+  }
+
+  let { name } = data;
   if (Kuroshiro.Util.hasJapanese(name)) {
     name = await converter(name);
     name = name.replace(/(^|\s)\S/g, t => t.toUpperCase());
@@ -21,4 +48,18 @@ async function showRomaji(uris) {
   Spicetify.showNotification(name);
 }
 
-new Spicetify.ContextMenu.Item(`Show Romaji`, showRomaji).register();
+function shouldAdd(uris) {
+  const uri = Spicetify.URI.fromString(uris[0]);
+  switch (uri.type) {
+    case Type.ALBUM:
+    case Type.TRACK:
+    case Type.ARTIST:
+    case Type.PLAYLIST:
+    case Type.PLAYLIST_V2:
+      return true;
+    default:
+      return false;
+  }
+}
+
+new Spicetify.ContextMenu.Item(`Show Romaji`, showRomaji, shouldAdd).register();
